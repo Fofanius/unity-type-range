13924f6888d023581cd81cf0e8101ea055961f4e
Fofanius <vova19007@gmail.com>
2024-03-05 21:20:01 +0200

    Added basic set of methods (Random Lerp Clamp)

--- Runtime/Range.cs
+++ Runtime/Range.cs
@@ -21,12 +21,37 @@ namespace Fofanius.Types
             set => _b = Mathf.Max(value, _a);
         }
 
+        /// <summary>
+        /// Length of the range.
+        /// </summary>
         public float Lenght => Mathf.Max(B - A, 0f);
 
         public Range(float a, float b)
         {
+            if (b < a) throw new ArgumentException($"Invalid arguments! B ({b}) cannot be less then A({a})!");
+
             _a = a;
             _b = b;
         }
+
+        /// <summary>
+        /// Generates random number in range [<see cref="Range.A"/>..<see cref="Range.B"/>] using <see cref="UnityEngine.Random.Range(float, float)"/>
+        /// </summary>
+        /// <returns>Generated random nubmer.</returns>
+        public float GetRandom() => UnityEngine.Random.Range(A, B);
+
+        /// <summary>
+        /// Clamps <paramref name="value"/> between <see cref="Range.A"/> and <see cref="Range.B"/> using <see cref="UnityEngine.Mathf.Clamp(float, float, float)"/>.
+        /// </summary>
+        /// <param name="value">Value to clamp</param>
+        /// <returns>Clamped value.</returns>
+        public float Clamp(float value) => Mathf.Clamp(value, A, B);
+
+        /// <summary>
+        /// Returns limearly interpolated value between <see cref="Range.A"/> and <see cref="Range.B"/> by <paramref name="t"/> using <see cref="UnityEngine.Mathf.Lerp(float, float, float)"/>.
+        /// </summary>
+        /// <param name="t">Interpolation value [0f..1f].</param>
+        /// <returns>Interpolation result.</returns>
+        public float Lerp(float t) => Mathf.Lerp(A, B, t);
     }
 }
