888aac877fd859529bf5dafd8bbc32a368dd59de
Fofanius <vova19007@gmail.com>
2024-03-05 21:22:10 +0200

    Added IEquatable implementation

--- Runtime/Range.cs
+++ Runtime/Range.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Fofanius.Types
 {
     [Serializable]
-    public struct Range
+    public struct Range : IEquatable<Range>
     {
         [SerializeField] private float _a;
         [SerializeField] private float _b;
@@ -53,5 +53,24 @@ namespace Fofanius.Types
         /// <param name="t">Interpolation value [0f..1f].</param>
         /// <returns>Interpolation result.</returns>
         public float Lerp(float t) => Mathf.Lerp(A, B, t);
+
+        #region IEquatable
+
+        public bool Equals(Range other)
+        {
+            return _a.Equals(other._a) && _b.Equals(other._b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Range other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_a, _b);
+        }
+
+        #endregion
     }
 }
